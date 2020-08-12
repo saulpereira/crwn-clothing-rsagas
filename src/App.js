888afc0a904581d7879dddf8c1bd94ce6d8a4efd
saulpereira <saul_pereira@hotmail.com>
2020-08-12 17:52:26 -0600

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,13 +31,10 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-
-          console.log(this.state);
         })
-      } else
-      {
+      } 
+      
         this.setState({currentUser: userAuth});
-      }
     })
   };
 
