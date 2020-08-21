f62a57ca84c66c05764fe7e1487f160aba46a658
saulpereira <saul_pereira@hotmail.com>
2020-08-21 15:10:00 -0600

    Adding Shop Data to Redux

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import {  setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+//import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -35,6 +36,8 @@ class App extends React.Component {
       } 
       
         setCurrentUser(userAuth);
+        //addCollectionAndDocuments("collections", collectionsArray);
+    //    //addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({ title, items })));
     })
   };
 
