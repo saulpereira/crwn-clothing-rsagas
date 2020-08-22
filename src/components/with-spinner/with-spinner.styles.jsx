a4be6ddbdb91de21a8d519a387bc30e61692a59d
saulpereira <saul_pereira@hotmail.com>
2020-08-22 16:12:29 -0600

    adding redux thunk

--- src/components/with-spinner/with-spinner.styles.jsx
+++ src/components/with-spinner/with-spinner.styles.jsx
@@ -9,22 +9,24 @@ export const SpinnerOverlay = styled.div`
 `;
 
 export const SpinnerContainer = styled.div`
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-  border: 3px solid rgba(195, 195, 195, 0.6);
-  border-radius: 50%;
-  border-top-color: #636767;
-  animation: spin 1s ease-in-out infinite;
-  -webkit-animation: spin 1s ease-in-out infinite;
-  @keyframes spin {
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    border: 3px solid rgba(195, 195, 195, 0.6);
+    border-radius: 50%;
+    border-top-color: #636767;
+    animation: spin 1s ease-in-out infinite;
+    -webkit-animation: spin 1s ease-in-out infinite;
+
+    @keyframes spin {
     to {
-      -webkit-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
     }
-  }
-  @-webkit-keyframes spin {
+    }
+    
+    @-webkit-keyframes spin {
     to {
-      -webkit-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
     }
-  }
+    }
 `;
